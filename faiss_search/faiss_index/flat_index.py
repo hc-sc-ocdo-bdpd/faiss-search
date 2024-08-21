c84826de45464270415416881bd4d1e75e35ad4e
TylerWilsonHC-SC <tyler.wilson@hc-sc.gc.ca>
2024-08-21 08:51:02 -0700

    copied over faiss index functionality

---
+++ faiss_search/faiss_index/flat_index.py
@@ -0,0 +1,14 @@
+import faiss
+import numpy as np
+from faiss_search.faiss_index.faiss_strategy import FAISSStrategy
+
+
+class FlatIndex(FAISSStrategy):
+    def _create_index(self, embeddings: np.ndarray, metric: int):
+        dimension = embeddings.shape[1]
+        index = faiss.IndexFlat(dimension, metric)
+        index.add(embeddings)
+        return index
+
+    def query(self, xq: np.ndarray, k: int = 1):
+        return super().query(xq, k)
