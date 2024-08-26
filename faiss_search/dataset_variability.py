6a627b9598db1b63a459e82d69bfdfc4ee0b0c75
TylerWilsonHC-SC <tyler.wilson@hc-sc.gc.ca>
2024-08-26 10:27:06 -0700

    added cosine similarity metric to dataset variability

--- faiss_search/dataset_variability.py
+++ faiss_search/dataset_variability.py
@@ -4,5 +4,9 @@ class DatasetVariability:
     def __init__(self, embedding: np.ndarray):
         self.embedding = embedding
 
-    def variance(self):
+    def variance(self) -> float:
         return np.sum(np.var(self.embedding, axis=0))
+    
+    def cosine_similarity_avg(self) -> float:
+        mean_vec = np.mean(self.embedding, axis=0)
+        return np.mean(np.dot(self.embedding, mean_vec))
